6308527b18db003bef55aeee60b5e5fbf2c28461
ceaston7 <ceaston7@hotmail.com>
2021-09-16 21:26:20 -0400

    Initial push, some walking code

---
+++ Assets/Scripts/MyUserSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace MyUserSettings{
+    public enum Locomotion{
+        blink,
+        slide,
+        walk
+    }
+
+    public class MyUserSettings
+    {
+        //Mode of movement choice
+        public static Locomotion LocomotionMode;
+
+        //Height of tracker above playspace floor when standing normally
+        public static float baseFootHeight;
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using MyUserSettings;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Transform leftFoot;
+    public Transform rightFoot;
+    public Transform camera;
+    Rigidbody rigidbody;
+    bool walkButtonIsDown;
+    Vector3 lastLeftFootPos = new Vector3();
+    Vector3 lastRightFootPos = new Vector3();
+    public SteamVR_Action_Boolean walkButton;
+    public SteamVR_Input_Sources controller;
+
+    void Start()
+    {
+        MyUserSettings.MyUserSettings.LocomotionMode = Locomotion.walk;
+        walkButtonIsDown = false;
+        rigidbody = GetComponent<Rigidbody>();
+        switch (MyUserSettings.MyUserSettings.LocomotionMode)
+        {
+            case Locomotion.blink:
+                break;
+            case Locomotion.slide:
+                break;
+            case Locomotion.walk:
+                Debug.Log("setting action");
+                //walking button
+                walkButton.AddOnChangeListener(walkButtonChange, controller);
+                break;
+        }
+    }
+
+    void Update()
+    {
+        if (walkButtonIsDown){
+            walk();
+        }
+        //update last foot positions after walk
+        lastLeftFootPos.Set(leftFoot.position.x, leftFoot.position.y, leftFoot.position.z);
+        lastRightFootPos.Set(rightFoot.position.x, rightFoot.position.y, rightFoot.position.z);
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    //TODO: Walking backward?
+    private void walk()
+    {
+        Debug.Log("In walk");
+        var camera2dForward = new Vector3(camera.forward.x, 0.0f, camera.position.z);
+        camera2dForward.Normalize();
+        Debug.Log("camera forward: " + camera2dForward.ToString("F3"));
+        //TODO: See if a minimum movement is required to keep from slowly walking while standing still
+        Debug.Log((camera2dForward * (Mathf.Abs(leftFoot.position.y - lastLeftFootPos.y) + Mathf.Abs(rightFoot.position.y - lastRightFootPos.y))).ToString("F3"));
+        transform.position += camera2dForward * (Mathf.Abs(leftFoot.position.y - lastLeftFootPos.y) + Mathf.Abs(rightFoot.position.y - lastRightFootPos.y));
+    }
+
+    void walkButtonChange(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource, bool newState){
+        walkButtonIsDown = newState;
+        Debug.Log("walkButton: " + walkButtonIsDown);
+    }
+}
