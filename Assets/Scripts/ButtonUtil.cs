a8804430d0cfa50629ff764f389320d13bc1093b
Christopher Easton <ceaston7@hotmail.com>
2021-09-30 10:06:04 -0400

    Menu manager finished, untested. Button script finished, untested, renamed to ButtonUtil

---
+++ Assets/Scripts/ButtonUtil.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonUtil : MonoBehaviour
+{
+    public Canvas nextMenu;
+
+    public void changeMenu(){
+        transform.root.GetComponent<MenuManager>().changeMenu(nextMenu);
+    }
+}
