a1dbad20e29bf5be2aa6fd1bc812ee7e789876ad
Christopher Easton <ceaston7@hotmail.com>
2021-10-09 12:36:31 -0400

    Calibration script

---
+++ Assets/Scripts/Calibration.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calibration : MonoBehaviour
+{
+    public float baseHeight;
+    private List<float> measurements;
+    private float calibrateTime = 5.0f;
+
+    void Awake(){
+        measurements = new List<float>();
+    }
+
+    public void CalibrateHeight(){
+        measurements = new List<float>();
+        RecordHeight();
+    }
+
+    public IEnumerator RecordHeight() {
+        for (float timer = 0.0f; timer < calibrateTime; timer += Time.deltaTime)
+        { 
+            Debug.Log("recording height");
+            measurements.Add(transform.position.y);
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
