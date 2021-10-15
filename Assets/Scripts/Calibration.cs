51397b7ff97ab479a89737ba9b72f31c3819452f
ceaston7 <ceaston7@hotmail.com>
2021-10-15 10:05:51 -0400

    Foot prefab

--- Assets/Scripts/Calibration.cs
+++ Assets/Scripts/Calibration.cs
@@ -4,26 +4,41 @@ using UnityEngine;
 
 public class Calibration : MonoBehaviour
 {
-    public float baseHeight;
+    public float baseHeight = 0;
     private List<float> measurements;
-    private float calibrateTime = 5.0f;
+    private float calibrateTime = 2.0f;
+    public bool recordingHeight = false;
 
     void Awake(){
         measurements = new List<float>();
     }
 
-    public void CalibrateHeight(){
-        measurements = new List<float>();
-        RecordHeight();
+    public void CalibrateHeight()
+    {
+        if (!recordingHeight) { 
+            measurements = new List<float>();
+            recordingHeight = true;
+            RecordHeight();
+        }
     }
 
     public IEnumerator RecordHeight() {
         for (float timer = 0.0f; timer < calibrateTime; timer += Time.deltaTime)
         { 
             Debug.Log("recording height");
-            measurements.Add(transform.position.y);
+            //Difference between tracker and floor height
+            measurements.Add(transform.position.y - transform.root.transform.position.y);
 
             yield return new WaitForFixedUpdate();
         }
+
+        float sum = 0;
+
+        foreach(var measurement in measurements){
+            sum += measurement;
+        }
+
+        baseHeight = sum / measurements.Count;
+        recordingHeight = false;
     }
 }
