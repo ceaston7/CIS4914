f6917658f5ae414bd8be4f40d5f4fc3608d8f415
ceaston7 <ceaston7@hotmail.com>
2021-11-07 18:29:44 -0500

    Demo scene update

---
+++ Assets/Scripts/DeactivateButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateButton : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> deactivate;
+    bool alreadyCollided = false;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(!alreadyCollided && collision.collider.CompareTag("Player")){
+            foreach(GameObject d in deactivate){
+                d.SetActive(false);
+            }
+            alreadyCollided = true;
+        }
+    }
+
+    public void OnCollisionExit(Collision collision){
+        if (collision.collider.CompareTag("Player"))
+            alreadyCollided = false;
+    }
+}
